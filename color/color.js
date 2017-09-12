b20c16241e523a5e5c75e75621cc41d50bd8b9f6
NixxonWTFU <31675525+NixxonWTFU@users.noreply.github.com>
2017-09-12 19:46:27 -0400

    changed pages

---
+++ color/color.js
@@ -0,0 +1,94 @@
+function setup()  {
+	createCanvas(700, 500);
+	var gray = color(80, 79, 90);
+    var night = color(10, 4, 21);
+    var yellow = color(255, 239, 162);
+    var darkgray = color(49, 49, 57);
+
+	background(night);
+	   /*back buildings*/
+	fill(darkgray);
+	noStroke();
+	rect(50, 100, 150, 400);
+
+	rect(200, 200, 200, 300);
+
+	rect(500, 50, 150, 450);
+
+	rect(75, 25, 7, 100);
+
+	rect(600, 300, 100, 200);
+
+
+
+
+
+
+      /*front buildings*/
+	fill(gray);
+	rect(0, 300, 150, 200);
+
+	rect(200, 250, 250, 250);
+
+	rect(450, 150, 150, 350);
+
+	/*window lights*/
+	fill(yellow);
+	rect(75, 150, 25, 50);
+
+	rect(150, 200, 25, 50);
+	
+	rect(75, 350, 25, 50);
+
+	rect(75, 450, 25, 50);
+
+	rect(225, 300, 25, 50);
+
+	rect(225, 400, 25, 50);
+
+	rect(300, 300, 25, 50);
+
+	rect(300, 400, 25, 50);
+
+	rect(400, 350, 25, 50);
+
+	rect(475, 350, 25, 50);
+
+	rect(550, 350, 25, 50);
+
+	rect(475, 200, 25, 50);
+
+	rect(550, 200, 25, 50);
+
+	rect(525, 100, 25, 50);
+
+	rect(600, 100, 25, 50);
+
+	/*moon*/
+	fill(yellow);
+	ellipse(275, 75, 100, 100);
+    /*stars*/
+	rect(50, 50, 3, 3);
+	rect(100, 75, 3, 3);
+	rect(25, 200, 3, 3);
+	rect(250, 150, 3, 3);
+	rect(300, 175, 3, 3);
+	rect(200, 50, 3, 3);
+	rect(350, 120, 3, 3);
+	rect(440, 80, 3, 3);
+	rect(400, 25, 3, 3);
+	rect(410, 200, 3, 3);
+	rect(460, 120, 3, 3);
+	rect(530, 40, 3, 3);
+	rect(560, 10, 3, 3);
+	rect(650, 25, 3, 3);
+	rect(670, 100, 3, 3);
+
+
+}
+
+function draw()  {
+
+
+	
+}
---
+++ selfportrait/sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+	createCanvas(400, 400);
+    var red = color("red");
+    var black = color("#000000");
+    var white = color("#ffffff");
+    var blue = color("blue");
+
+    background(0);
+
+    fill(red);
+    ellipse(200, 200, 250, 300);
+    
+    fill(black);
+    ellipse(140, 190, 90, 140);
+    ellipse(260, 190, 90, 140);
+    
+    line(140, 70, 140, 330);
+    line(260, 70, 260, 330);
+    
+    fill(white)
+    ellipse(140, 190, 40, 60);
+    ellipse(260, 190, 40, 60);
+}
+
+
+function draw() {
+	
+}
